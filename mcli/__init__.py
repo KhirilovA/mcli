6734ab2a4b1680d586b40ea8a0687efd8c84fa60
Artur Aliiev <artur.aliiev@magnise.com.ua>
2022-04-15 12:38:34 +0300

    refactor cli

--- mcli/__init__.py
+++ mcli/__init__.py
@@ -1,42 +1,14 @@
+import json
+import fire
 from mcli.engine.inspect import ViewInspector
 from pyfiglet import Figlet
 
-if __name__ == '__main__':
-    f = Figlet(font='slant')
-    print(f.renderText('QBCLI'))
-    args_help = {
-        "db_name": "? Name of database: ",
-        "db_host": "? Host of database conn: ",
-        "db_port": "? Port of database conn: ",
-        "db_user": "? Username: ",
-        "db_password": "? Password: ",
-        "db_schema": "? Name of DB schema: ",
-        "sql_module": "? Dotted name of sql-package: (ex: bb.sql) ",
-        "sql_full_path": "? Full path to sql-package: ",
-        "sql_name": "? Name of sql-file? ",
-        "root_folder": "? Full path to Root Folder: ",
-        "view_name": "? Name of materialized view: ",
-        "create_index": "Need to create an index? [y,n] ",
-        "index_name": "? Index name: ",
-        "index_column": "? Columns for index: (ex: id,name,surname) ",
-        "render_model": "Need to create endpoint boilerplate? [y/n] ",
-        "url": "? URL of endpoint: ",
-        "module_name": "kpi_newsletter",
-        "api_class_name_pascal_case": "? Name of cls in Pascal Case: ",
-        "api_class_name_snake_case": "? Name of cls in Snake Case: ",
 
-    }
-    args_values = {k: None for k, v in args_help.items()}
+def create_view(config):
+    with open(config, "r") as file:
+        settings = json.load(file)
+        args_values = settings['config']['value']
 
-    for k, v in args_help.items():
-        match k:
-
-            case "db_port":
-                args_values[k] = int(input(v))
-            case "create_index", "render_model":
-                args_values[k] = True if input(v).lower().strip() != "n" else False
-            case _:
-                args_values[k] = input(v)
     db_url = f"postgresql+psycopg2://{args_values['db_user']}:{args_values['db_password']}" \
              f"@{args_values['db_host']}:{args_values['db_port']}/{args_values['db_name']}"
     ViewInspector(db_url=db_url,
@@ -54,3 +26,9 @@ if __name__ == '__main__':
                                   api_class_name_pascal_case=args_values['api_class_name_pascal_case'],
                                   root_name=args_values['root_folder'],
                                   schema_name=args_values['db_schema'])
+
+
+if __name__ == '__main__':
+    f = Figlet(font='slant')
+    print(f.renderText('MCLI!'))
+    fire.Fire(create_view)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=['mcli', 'mcli.engine'],
     url='https://github.com/Skadi0h/mcli.git',
-    license='',
+    license='MIT',
     author='Skadi0h',
     author_email='artur.aliiev@magnise.com.ua',
     description='Boiler plate for QueryBuilder'
