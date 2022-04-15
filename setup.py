925d1f29865019c32c85d6c8cf39a85d884fc8c0
Artur Aliiev <artur.aliiev@magnise.com.ua>
2022-04-15 08:45:36 +0300

    refactor name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='mat_view_cli',
+    name='mcli',
     version='0.1.0',
     packages=['mcli', 'mcli.engine'],
     url='https://github.com/Skadi0h/mat_view_cli.git',
     license='',
     author='Skadi0h',
     author_email='artur.aliiev@magnise.com.ua',
-    description=''
+    description='Boiler plate for QueryBuilder'
 )
