a0e2295b286ce390ea1b76c71ceb959af63a6481
Artur Aliiev <artur.aliiev@magnise.com.ua>
2022-04-15 08:35:22 +0300

    rm poetry lock

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mat_view_cli',
+    version='0.1.0',
+    packages=['cli', 'cli.engine'],
+    url='https://github.com/Skadi0h/mat_view_cli.git',
+    license='',
+    author='Skadi0h',
+    author_email='artur.aliiev@magnise.com.ua',
+    description=''
+)
