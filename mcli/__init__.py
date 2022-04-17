46d764d7a9f20a9cacc4bb0eedc1cb27cbe23c88
Artur Aliiev <artur.aliiev@magnise.com.ua>
2022-04-17 17:04:48 +0300

    refactor cli

--- mcli/__init__.py
+++ mcli/__init__.py
@@ -5,6 +5,17 @@ from mcli.engine.render_model import ModelRenderer
 from mcli.engine.models import ConfigModel
 
 
+def load_config(path: str):
+    with open(path, "r") as file:
+        settings = json.load(file)
+        args_values = settings['config']['data']
+
+    db_url = f"postgresql+psycopg2://{args_values['db_user']}:{args_values['db_password']}" \
+             f"@{args_values['db_host']}:{args_values['db_port']}/{args_values['db_name']}"
+    args_values.update({"db_url": db_url})
+    return ConfigModel(**args_values)
+
+
 @click.group()
 def click_group():
     pass
@@ -15,15 +26,8 @@ def click_group():
               help='JSON config of mcli')
 @click.command()
 def create_view(config):
-    with open(config, "r") as file:
-        settings = json.load(file)
-        args_values = settings['config']['data']
-
-    db_url = f"postgresql+psycopg2://{args_values['db_user']}:{args_values['db_password']}" \
-             f"@{args_values['db_host']}:{args_values['db_port']}/{args_values['db_name']}"
-    args_values.update({"db_url": db_url})
-    config = ConfigModel(**args_values)
-    ViewInspector(config=config).register_view()
+    cfg = load_config(config)
+    ViewInspector(config=cfg).register_view()
 
 
 @click.option('--config', default="mcli_config.json",
@@ -31,13 +35,15 @@ def create_view(config):
 @click.option('--view_name', prompt='Name of view to delete')
 @click.command()
 def delete_view(config, view_name: str):
-    ViewInspector(config=config).delete_view(view_name)
+    cfg = load_config(config)
+    ViewInspector(config=cfg).delete_view(view_name)
 
 
 @click.option('--config', default="mcli_config.json",
               help='JSON config of mcli')
 @click.command()
 def validate_views(config):
+    cfg = load_config(config)
     pass
 
 
@@ -45,4 +51,5 @@ def validate_views(config):
               help='JSON config of mcli')
 @click.command()
 def create_model(config):
-    ModelRenderer(config=config).create_module()
+    cfg = load_config(config)
+    ModelRenderer(config=cfg).create_module()
