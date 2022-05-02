a30adc64b7405899bd63b4f5340bb2457d6fa080
Artur Aliiev <artur.aliiev@magnise.com.ua>
2022-05-02 18:46:25 +0300

    ** fix config option **

--- mcli/__init__.py
+++ mcli/__init__.py
@@ -5,6 +5,7 @@ from mcli.engine.models import ConfigModel
 from mcli.engine.manage_mvc import MVCTemplateManager
 from mcli.engine.manage_view import ViewManager
 
+
 def load_config(path: str):
     with open(path, "r") as file:
         settings = json.load(file)
@@ -24,12 +25,18 @@ def click_group(config):
     pass
 
 
+@click.option('--config',
+              default="mcli_config.json",
+              help='JSON config of mcli')
 @click.command()
 def create_views(config):
     cfg = load_config(config)
     ViewManager(config=cfg).create_views()
 
 
+@click.option('--config',
+              default="mcli_config.json",
+              help='JSON config of mcli')
 @click.command()
 def create_mvc(config):
     cfg = load_config(config)
