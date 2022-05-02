128ff708c30a8e64bdf1c4e82e9d312a5ada8a20
Artur Aliiev <artur.aliiev@magnise.com.ua>
2022-05-02 19:45:49 +0300

    ** fix format templates **

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mcli',
-    version='2.2',
+    version='2.3',
     packages=['mcli', 'mcli.engine'],
     url='https://github.com/Skadi0h/mcli.git',
     license='MIT',
