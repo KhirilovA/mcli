55e3bf3d3ad120acc7d67f3c2045292503e28e08
Andrii Khirilov <khirilov.and@gmail.com>
2022-05-17 18:23:40 +0300

    Updated Fastapi

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='mcli',
-    version='3.9',
+    version='3.10',
     packages=['mcli', 'mcli.engine'],
-    url='https://github.com/Skadi0h/mcli.git',
+    url='https://github.com/KhirilovA/mcli.git',
     license='MIT',
-    author='Skadi0h',
+    author='KhirilovA',
     author_email='artur.aliiev@magnise.com.ua',
     description='Boiler plate for QueryBuilder'
 )
